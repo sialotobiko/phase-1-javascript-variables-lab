ef29a05e961219c202555c138c0fac3692590622
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-08-17 00:55:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+var companyName = 'Scuber';
+const companyNameNew = 'Scuber';
+
+var mostProfitableNeighborhood ='Chelsea';
+let mostProfitableNeighborhoodNew = 'Chelsea';
+
+var companyCeo = 'Susan Smith';
+let companyCeoNew = 'Susan Smith';
